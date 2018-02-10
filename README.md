9cc1d00d347324008e526ad49bf0c13b463a701e
Jörgen <joergen.brandt@onlinehome.de>
2018-02-10 23:07:14 +0100

    Boilerplate code added. Travis support enabled.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cf_client
-A Cuneiform client implementation.
+###### A Cuneiform client implementation.
+
+[![Build Status](https://travis-ci.org/joergen7/cf_client.svg?branch=master)](https://travis-ci.org/joergen7/cf_client)
--- src/cf_client.erl
+++ src/cf_client.erl
@@ -1,7 +1,33 @@
-%%%-------------------------------------------------------------------
-%% @doc cf_client public API
+%% -*- erlang -*-
+%%
+%% A Cuneiform client implementation.
+%%
+%% Copyright 2015-2018 Jörgen Brandt
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% -------------------------------------------------------------------
+%% @author Jörgen Brandt <joergen.brandt@onlinehome.de>
+%% @version 0.1.0
+%% @copyright 2015-2018 Jörgen Brandt
+%%
+%%
+%%
+%%
+%%
 %% @end
-%%%-------------------------------------------------------------------
+%% -------------------------------------------------------------------
+
 
 -module( cf_client ).
 -behaviour( application ).
@@ -60,7 +86,8 @@ start( _StartType, _StartArgs ) ->
 
     end,
 
-  error_logger:info_report( [{application, cf_worker},
+  error_logger:info_report( [{info,        "ready to start supervision tree"}
+                             {application, cf_client},
                              {node,        node()},
                              {cre_node,    CreNode}] ),
 
