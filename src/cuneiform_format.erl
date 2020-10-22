6913dbeae3e091ecb76cd48a7a2dd06d1a1146dc
Jörgen Brandt <joergen@cuneiform-lang.org>
2020-10-22 09:02:39 +0200

    remove no_list_type error condition

--- src/cuneiform_format.erl
+++ src/cuneiform_format.erl
@@ -557,10 +557,6 @@ format_error( {error, type, {tl_no_list, Info, {E, T}}} ) ->
 
 % TODO: continue here  
 
-format_error( {error, type, {no_list_type, Info, T}} ) ->
-  io_lib:format( "type error ~s: list expected, got ~s",
-                 [format_info( Info ), format_type( T )] );
-
 format_error( {error, type, {no_comparable_type, Info, T}} ) ->
   io_lib:format( "type error ~s: type not comparable ~s",
                  [format_info( Info ), format_type( T )] );
